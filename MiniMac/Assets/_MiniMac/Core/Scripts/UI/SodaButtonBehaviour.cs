b359d6e2d37a4b26fb7a99d46a03076b4cc84eed
Lohannes <lohannes.s26@gmail.com>
2026-09-15 18:08:38 -0300

    Adicionado Efeito para os botões que interagem com o shader de bolhas

---
+++ MiniMac/Assets/_MiniMac/Core/Scripts/UI/SodaButtonBehaviour.cs
@@ -0,0 +1,97 @@
+using System;
+using DG.Tweening;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Image = UnityEngine.UI.Image;
+
+public class SodaButtonBehaviour : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private Image buttonImage;
+    private Material currentMaterial;
+    private RectTransform rect;
+
+    [Header("Bubble Speed Settings")]
+    [SerializeField] float defaultSpeed = 0.05f;
+    [SerializeField] float acceleratedSpeed = 0.8f;
+
+    private Vector3 defaultScale;
+    private Vector3 pressedScale;
+    private Vector3 currentScale;
+
+    private Color32 defaultColor;
+    [SerializeField] Color32 pressedColor = Color.red;
+    private Color32 currentColor;
+
+    [SerializeField] float accelerationTime = 0.2f;
+    [SerializeField] float easeTime = 0.2f;
+
+    [SerializeField] Ease accelerationEase = Ease.OutQuad;
+
+    private float currentSpeed;
+    private float currentOffset;
+    private Tweener speedTween;
+    private Tweener sizeTween;
+    private Tweener colorTween;
+
+    private readonly string offsetReference = "_BubbleOffset";
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        currentMaterial = new(buttonImage.material);
+        buttonImage.material = currentMaterial;
+        rect = GetComponent<RectTransform>();
+
+        currentSpeed = defaultSpeed;
+        
+        defaultScale = rect.localScale;
+        pressedScale = defaultScale * 0.9f;
+        
+        defaultColor = buttonImage.color;
+
+        Vector2 proportion = new(rect.rect.width / rect.rect.height, 1f);
+        currentMaterial.SetVector("_AspectRatio", proportion);
+    }
+
+    void Update()
+    {
+        currentOffset += currentSpeed * Time.deltaTime;
+        currentMaterial.SetFloat(offsetReference, currentOffset);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        AudioManager.Instance.PlaySFX(SFXType.UI_Button_Click);
+
+        speedTween?.Kill();
+        sizeTween?.Kill();
+        colorTween?.Kill();
+
+        speedTween = DOTween.To(() => currentSpeed, x => currentSpeed = x, acceleratedSpeed, accelerationTime)
+            .SetEase(accelerationEase);
+
+        sizeTween = rect.DOScale(pressedScale, easeTime)
+            .SetEase(accelerationEase);
+
+        colorTween = buttonImage.DOColor(pressedColor, easeTime)
+            .SetEase(accelerationEase);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        speedTween?.Kill();
+        sizeTween?.Kill();
+        colorTween?.Kill();
+
+        speedTween = DOTween.To(() => currentSpeed, x => currentSpeed = x, defaultSpeed, accelerationTime)
+            .SetEase(Ease.InOutSine);
+
+        sizeTween = rect.DOScale(defaultScale, easeTime)
+            .SetEase(Ease.OutBack); 
+
+        colorTween = buttonImage.DOColor(defaultColor, easeTime)
+            .SetEase(Ease.InOutSine);
+    }
+}
